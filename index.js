47a3e6fbdbe30a3cd40a6071566ce5b18ea71512
Yiyu He <dead-horse@users.noreply.github.com>
2017-03-23 17:32:07 +0800

    feat: support await(emitter, event) (#4)

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict'
 
-module.exports = function(event) {
+module.exports = function(emitter, event) {
+  if (typeof emitter === 'string') {
+    event = emitter
+    emitter = this
+  }
+
   return new Promise((resolve, reject) => {
     const done = event === 'error' ? reject : resolve
-    this.once(event, done)
+    emitter.once(event, done)
   })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -26,3 +26,23 @@ it('await error', done => {
     done()
   })
 })
+
+it('await(emitter, event)', () => {
+  return co(function* () {
+    const e = new EventEmitter()
+    setTimeout(() => e.emit('data', 'foo'), 1000)
+    assert.equal(yield awaitEvent(e, 'data'), 'foo')
+  })
+})
+
+it('await(emitter, error)', done => {
+  co(function* () {
+    const e = new EventEmitter()
+    setTimeout(() => e.emit('error', new Error('error')), 1000)
+
+    yield awaitEvent(e, 'error')
+  }).catch(err => {
+    assert.equal(err.message, 'error')
+    done()
+  })
+})
