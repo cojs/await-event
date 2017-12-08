8f31bdb0ea7f8c5249fdc99891d4bb4c09c991ea
Hongcai Deng <admin@dhchouse.com>
2017-12-08 16:56:48 +0800

    feat: support timeout

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict'
 
-module.exports = function(emitter, event) {
+module.exports = function(emitter, event, timeout) {
   if (typeof emitter === 'string') {
+    timeout = event
     event = emitter
     emitter = this
   }
 
   return new Promise((resolve, reject) => {
     const done = event === 'error' ? reject : resolve
+    if (timeout) {
+      setTimeout(reject, timeout, new Error('timeout'))
+    }
     emitter.once(event, done)
   })
 }
